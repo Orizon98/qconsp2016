282cf8388a5cd4e8c8b6c4207d87c76d828c938c
feroult <feroult@gmail.com>
2016-03-23 08:53:01 -0300

    qconsp2016

--- src/main/java/qconsp/models/order/OrderShield.java
+++ src/main/java/qconsp/models/order/OrderShield.java
@@ -1,11 +1,23 @@
 package qconsp.models.order;
 
+import io.yawp.commons.http.annotation.GET;
 import io.yawp.repository.IdRef;
 import io.yawp.repository.shields.Shield;
 
 import java.util.List;
+import java.util.Map;
 
 public class OrderShield extends Shield<Order> {
+
+    @Override
+    public void index(IdRef<?> parentId) {
+        allow();
+    }
+
+    @Override
+    public void show(IdRef<Order> id) {
+        allow();
+    }
 
     @Override
     public void create(List<Order> objects) {
@@ -14,6 +26,11 @@ public class OrderShield extends Shield<Order> {
 
     @Override
     public void update(IdRef<Order> id, Order object) {
+        allow();
+    }
+
+    @GET
+    public void nextBatch(Map<String, String> params) {
         allow();
     }
 }
