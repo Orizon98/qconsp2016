afe62c4508cb46f6ce3f8a8e614b7b77f49f8727
feroult <feroult@gmail.com>
2016-03-25 13:02:28 -0300

    qconsp2016

--- scripts/update-orders.js
+++ scripts/update-orders.js
@@ -2,7 +2,7 @@ module.exports = (function () {
 
     const BACKEND_API = process.env.QCON2016_API;
 
-    const BATCH_SIZE = 400;
+    const BATCH_SIZE = 1000;
 
     var async = require('async');
     var yawp = require('./lib/yawp');
@@ -64,7 +64,7 @@ module.exports = (function () {
                 logBatchThroughput();
                 batchDone = 0;
 
-                if (loaded - done <= 3 * BATCH_SIZE) {
+                if (loaded - done <= 4 * BATCH_SIZE) {
                     loadMoreOrders();
                 }
             }
